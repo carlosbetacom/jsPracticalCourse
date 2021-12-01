e430a58eb0cee667a2599c50f676ef964a28d7ae
Carlos Beta <betancourt0210@gmail.com>
2021-12-01 14:45:05 -0600

    HTML necesario para el layout y funciones JS para el funcionamiento del calculo del perimetro y area del cuadrado

--- js/figures.js
+++ js/figures.js
@@ -13,3 +13,45 @@ function showFormFigure() {
     if(idName != '0') elementFigure.classList.remove('hide');
 
 }
+
+function calculateSquare() {
+
+    const sideSquare = String(document.getElementById('sideSquare').value);
+
+    // Calculating perimeter and area
+    let perimeterSquare = sideSquare * 4;
+    let areaSquare = sideSquare * sideSquare;
+
+    // Write results
+    let perimeterSquareElement = document.getElementById('perimeterSquare');
+    let areaSquareElement = document.getElementById('areaSquare');
+
+    // Showing results
+    perimeterSquareElement.innerHTML = String(perimeterSquare);
+    areaSquareElement.innerHTML = String(areaSquare);
+
+}
+
+/*
+Triangle:
+
+const sideTriangle1;
+const sideTriangle2;
+const baseTriangle;
+const heightTriangle;
+
+let perimeterTriangle = sideTriangle1 + sideTriangle2 + baseTriangle;
+let areaTriangle = (baseTriangle * heightTriangle) / 2;
+*/
+
+/*
+Circle:
+
+const radio;
+const diameter = radio * 2;
+const PI = Math.PI;
+
+let circunference = diameter * PI;
+let areaCircle = (radio * radio) * PI;
+
+*/
